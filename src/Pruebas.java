30c060440300014a1d3b00eebe9422609fce2a2a
sharlisbg <sharislizza15@gmail.com>
2023-03-29 19:11:45 -0600

    Cambios

---
+++ src/Pruebas.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import java.lang.Exception;
+
+/**
+ * @author Sharis Barrios
+ *
+ */
+
+public class Pruebas {
+
+    @Test
+	void test() {
+
+		
+		
+	}
+    
+}
